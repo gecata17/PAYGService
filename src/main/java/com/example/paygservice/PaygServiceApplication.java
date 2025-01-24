6008d77d81c7eff191be69f049a18e668d1321b8
Georgi Birdanov <gogobiserov@gmail.com>
2025-01-25 00:47:06 +0200

    Initial commit

---
+++ src/main/java/com/example/paygservice/PaygServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.paygservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaygServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaygServiceApplication.class, args);
+    }
+
+}
