2e2ef544156ae70853d5c9a561564165b636f51d
Georgi Birdanov <gogobiserov@gmail.com>
2025-01-25 02:56:20 +0200

    added controllers,model class for user, configured registration and login , added hash algorithm for password,added jwt token generator for authenticated users and linked our database postgre to the backend via hibernate, added needed spring,lombok and postgre dependencies

---
+++ src/main/java/com/example/paygservice/service/UserServiceImplementation.java
@@ -0,0 +1,91 @@
+package com.example.paygservice.service;
+
+import com.example.paygservice.config.UserDetailsImplementation;
+import com.example.paygservice.model.User;
+import com.example.paygservice.repository.UserRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImplementation implements UserService, UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void createUser(User user) {
+
+        if (userRepository.existsById(user.getUsername())) {
+            throw new IllegalArgumentException("Username already in use");
+        }
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(newUser);
+
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findById(username).orElseThrow(EntityExistsException::new);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(userRepository.findAll());
+    }
+
+    @Override
+    public User updateUser(User updatedUser, String username) {
+
+        if (!username.equals(updatedUser.getUsername()) && userRepository.existsById(updatedUser.getUsername())) {
+            throw new EntityExistsException("Username already in use");
+        }
+
+
+        User existingUser = userRepository.findById(username)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+
+        existingUser.setEmail(updatedUser.getEmail());
+
+
+        return userRepository.save(existingUser);
+    }
+
+
+    @Override
+    public void deleteByUsername(String username) {
+        userRepository.deleteByUsername(username);
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findById(username).orElseThrow(EntityExistsException::new);
+        System.out.println(user.toString());
+        return new UserDetailsImplementation(user);
+    }
+}
